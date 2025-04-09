1dd14deb9562b29486a11d3cfae9c8bc6204b7a6
NATTHAPAT TECHAPORNHIRAN <natthapat.tec@student.mahidol.edu>
2025-04-09 09:49:06 +0000

    srry

--- main.cpp
+++ main.cpp
@@ -34,7 +34,17 @@ class Product {
         int stock;
     public:
         Product(string name,float price,int stock):name(name),price(price),stock(stock) {}
-    
+        void displayProduct() {
+            cout << "Product: " << name << ", Price: $" << price
+             << ", Stock: " << stock << endl;
+        }
+    string getName() {return name; }
+    float getPrice() { return price; }
+    int getStock() { return stock; }
+
+    string getLine() {
+        return name + "," + to_string(price) + "," + to_string(stock);
+    }
 };
 
 
